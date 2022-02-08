4cf7598498421baa3f592c4c9fc229f8b0206192
B3N <b@akhras.at>
2022-02-08 16:47:11 +0100

    add jwt support (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,57 @@
-import {Fabric} from "./fabric";
+import * as jwt from 'jsonwebtoken';
+
+type Creds = {
+    target: string,
+    username: string | null,
+    password: string | null
+}
 
 const urlSearchParams = new URLSearchParams(window.location.search);
-const urlPathTarget   = window.location.pathname.split('/')[1];
+const hasAuth: boolean = urlSearchParams.has("auth");
+const hasTarget: boolean = urlSearchParams.has("target");
 
-let target: string | null = urlSearchParams.get('target');
-const username: string | null = urlSearchParams.get('username');
-const password: string | null = urlSearchParams.get('password');
-if (target == null) {
-    target = urlPathTarget;
+function credsViaJWT(): Creds {
+    const auth: string = urlSearchParams.get("auth") as string;
+    console.assert(typeof auth === 'string');
+    return jwt.decode(auth) as Creds;
 }
-if (target != null) {
-    console.log('[MAIN] configuration option passed by url', {
+
+function credsViaGet(): Creds {
+    const target: string = urlSearchParams.get("target") as string;
+    console.assert(typeof target === 'string');
+    return {
         target,
-        username,
-        password
-    });
-    const fabric = new Fabric('#fabric', target, username, password);
+        username: urlSearchParams.get("username"),
+        password: urlSearchParams.get("password")
+    }
 }
 
-export {}
+function credsViaPath(): Creds {
+    const pathTarget: string = window.location.pathname.split("/")[1];
+    return {
+        target: pathTarget,
+        username: null,
+        password: null
+    };
+}
+
+let target: string, username: string | null, password: string | null;
+
+if (hasAuth) {
+    ({ target, username, password } = credsViaJWT());
+} else if (hasTarget) {
+    ({ target, username, password } = credsViaGet());
+} else {
+    ({ target, username, password } = credsViaPath());
+}
+
+class Fabric {
+    constructor(
+        public target: string,
+        public username: string | null,
+        public password: string | null
+    ) {}
+}
+
+const fabric = new Fabric(target, username, password);
+console.log(fabric);
