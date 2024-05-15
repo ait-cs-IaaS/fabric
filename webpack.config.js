00bdd32cef939ff167c12bb7ae40d5d5a0ccfed8
Benjamin Akhras <b@akhras.at>
2024-05-15 14:25:07 +0200

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
@@ -11,6 +12,12 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'index.html'),
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, 'styles.css'), to: path.resolve(__dirname, 'dist') },
+                { from: path.resolve(__dirname, 'ux.js'), to: path.resolve(__dirname, 'dist') }
+            ]
         })
     ],
     module: {
